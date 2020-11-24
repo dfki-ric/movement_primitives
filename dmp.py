188bb97a20e7b0a8e4702f5ae80b888f0bcdaf5c
Alexander Fabisch <afabisch@googlemail.com>
2020-11-24 15:59:34 +0100

    Prototype of state-following DMP

--- dmp.py
+++ dmp.py
@@ -903,6 +903,55 @@ def dmp_open_loop_quaternion(goal_t, start_t, dt, start_y, goal_y, alpha_y, beta
     return np.asarray(T), np.asarray(Y)
 
 
+class StateFollowingForcingTerm:
+    def __init__(self, n_dims, n_viapoints, goal_t, start_t, overlap, alpha_z):
+        if n_viapoints <= 0:
+            raise ValueError("The number of viapoints must be > 0!")
+        self.n_viapoints = n_viapoints
+        if start_t >= goal_t:
+            raise ValueError("Goal must be chronologically after start!")
+        self.goal_t = goal_t
+        self.start_t = start_t
+        self.overlap = overlap
+        self.alpha_z = alpha_z
+
+        self.viapoints = np.zeros((n_viapoints, n_dims))
+
+        self._init_rbfs(n_viapoints, start_t)
+
+    def _init_rbfs(self, n_viapoints, start_t):
+        self.log_overlap = -np.log(self.overlap)
+        self.execution_time = self.goal_t - self.start_t
+        self.centers = np.empty(n_viapoints)
+        self.widths = np.empty(n_viapoints)
+        step = self.execution_time / self.n_viapoints
+        # do first iteration outside loop because we need access to i and i - 1 in loop
+        t = start_t
+        self.centers[0] = phase(t, self.alpha_z, self.goal_t, self.start_t)
+        for i in range(1, self.n_viapoints):
+            t = i * step  # normally lower_border + i * step but lower_border is 0
+            self.centers[i] = phase(t, self.alpha_z, self.goal_t, self.start_t)
+            # Choose width of RBF basis functions automatically so that the
+            # RBF centered at one center has value overlap at the next center
+            diff = self.centers[i] - self.centers[i - 1]
+            self.widths[i - 1] = self.log_overlap / diff ** 2
+        # Width of last Gaussian cannot be calculated, just use the same width as the one before
+        self.widths[self.n_viapoints - 1] = self.widths[self.n_viapoints - 2]
+
+    def _activations(self, z, normalized):
+        z = np.atleast_2d(z)  # 1 x n_steps
+        squared_dist = (z - self.centers[:, np.newaxis]) ** 2
+        activations = np.exp(-self.widths[:, np.newaxis] * squared_dist)
+        if normalized:
+            activations /= activations.sum(axis=0)
+        return activations
+
+    def __call__(self, t, int_dt=0.001):
+        z = phase(t, alpha=self.alpha_z, goal_t=self.goal_t, start_t=self.start_t, int_dt=int_dt)
+        z = np.atleast_1d(z)
+        return self._activations(z, normalized=True).T
+
+
 class StateFollowingDMP:
     def __init__(self, n_dims, execution_time, dt=0.01, n_viapoints=10, int_dt=0.001):
         self.n_dims = n_dims
@@ -916,6 +965,9 @@ class StateFollowingDMP:
 
         self.alpha_y = 25.0
         self.beta_y = self.alpha_y / 4.0
+
+        self.forcing_term = StateFollowingForcingTerm(
+            self.n_dims, self.n_viapoints, self.execution_time, 0.0, 0.1, alpha_z)
 
         self.last_t = None
         self.t = 0.0
@@ -962,19 +1014,20 @@ class StateFollowingDMP:
             self.start_y, self.start_yd, self.start_ydd,
             self.execution_time, 0.0,
             self.alpha_y, self.beta_y,
+            forcing_term=self.forcing_term,
             coupling_term=coupling_term,
             int_dt=self.int_dt)
         return current_y, current_yd
 
     def open_loop(self, run_t=None, coupling_term=None):
-        return state_following_dmp_open_loop(self.execution_time, 0.0, self.dt, self.start_y, self.goal_y, self.alpha_y, self.beta_y, coupling_term, run_t, self.int_dt)
+        return state_following_dmp_open_loop(self.execution_time, 0.0, self.dt, self.start_y, self.goal_y, self.alpha_y, self.beta_y, self.forcing_term, coupling_term, run_t, self.int_dt)
 
     def imitate(self, T, Y, regularization_coefficient=0.0,
                 allow_final_velocity=False):
         raise NotImplementedError()
 
 
-def state_following_dmp_step(last_t, t, last_y, last_yd, goal_y, goal_yd, goal_ydd, start_y, start_yd, start_ydd, goal_t, start_t, alpha_y, beta_y, coupling_term=None, coupling_term_precomputed=None, int_dt=0.001):
+def state_following_dmp_step(last_t, t, last_y, last_yd, goal_y, goal_yd, goal_ydd, start_y, start_yd, start_ydd, goal_t, start_t, alpha_y, beta_y, forcing_term, coupling_term=None, coupling_term_precomputed=None, int_dt=0.001):
     if start_t >= goal_t:
         raise ValueError("Goal must be chronologically after start!")
 
@@ -1002,13 +1055,16 @@ def state_following_dmp_step(last_t, t, last_y, last_yd, goal_y, goal_yd, goal_y
             cdd += coupling_term_precomputed[1]
 
         # TODO modify acceleration to incorporate viapoints
-        ydd = (alpha_y * (beta_y * (goal_y - y) + execution_time * goal_yd - execution_time * yd) + goal_ydd * execution_time ** 2 + cdd) / execution_time ** 2
-        yd += dt * ydd + cd / execution_time
+        h = forcing_term(current_t).squeeze(axis=0)
+
+        ydd = np.sum(h[:, np.newaxis] * alpha_y * (beta_y * (forcing_term.viapoints - y) - 0.5 * execution_time * yd[np.newaxis]) / (0.5 * execution_time) ** 2, axis=0)
+        ydd += cdd / (0.5 * execution_time) ** 2
+        yd += dt * ydd + cd / (0.5 * execution_time)
         y += dt * yd
     return y, yd
 
 
-def state_following_dmp_open_loop(goal_t, start_t, dt, start_y, goal_y, alpha_y, beta_y, coupling_term=None, run_t=None, int_dt=0.001):
+def state_following_dmp_open_loop(goal_t, start_t, dt, start_y, goal_y, alpha_y, beta_y, forcing_term, coupling_term=None, run_t=None, int_dt=0.001):
     t = start_t
     y = np.copy(start_y)
     yd = np.zeros_like(y)
@@ -1024,7 +1080,7 @@ def state_following_dmp_open_loop(goal_t, start_t, dt, start_y, goal_y, alpha_y,
             goal_y=goal_y, goal_yd=np.zeros_like(goal_y), goal_ydd=np.zeros_like(goal_y),
             start_y=start_y, start_yd=np.zeros_like(start_y), start_ydd=np.zeros_like(start_y),
             goal_t=goal_t, start_t=start_t,
-            alpha_y=alpha_y, beta_y=beta_y, coupling_term=coupling_term, int_dt=int_dt)
+            alpha_y=alpha_y, beta_y=beta_y, forcing_term=forcing_term, coupling_term=coupling_term, int_dt=int_dt)
         T.append(t)
         Y.append(np.copy(y))
     return np.asarray(T), np.asarray(Y)
--- examples/plot_state_following_dmp.py
+++ examples/plot_state_following_dmp.py
@@ -3,14 +3,23 @@ import numpy as np
 from dmp import StateFollowingDMP
 
 
-start_y = np.array([0.0])
-goal_y = np.array([1.0])
+start_y = np.array([0.0, 1.0])
 dt = 0.001
+execution_time = 3.0
+n_viapoints = 8
 
-dmp = StateFollowingDMP(n_dims=1, execution_time=1.0, dt=0.01, n_viapoints=10)
-dmp.configure(start_y=start_y, goal_y=goal_y)
+dmp = StateFollowingDMP(n_dims=2, execution_time=execution_time, dt=dt, n_viapoints=n_viapoints)
+dmp.forcing_term.viapoints[:, 0] = np.linspace(0, 1, n_viapoints)
+dmp.forcing_term.viapoints[:, 1] = np.linspace(1, 2, n_viapoints)
+dmp.configure(start_y=start_y)
 
-T, Y = dmp.open_loop()
+T, Y = dmp.open_loop(run_t=1.5 * execution_time)
 
-plt.plot(T, Y)
+ax = plt.subplot(121)
+ax.plot(T, Y)
+ax.scatter(np.linspace(0, execution_time, n_viapoints), dmp.forcing_term.viapoints[:, 0])
+ax.scatter(np.linspace(0, execution_time, n_viapoints), dmp.forcing_term.viapoints[:, 1])
+ax = plt.subplot(122)
+ax.plot(Y[:, 0], Y[:, 1])
+ax.scatter(dmp.forcing_term.viapoints[:, 0], dmp.forcing_term.viapoints[:, 1])
 plt.show()
