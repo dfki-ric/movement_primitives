707cdf899d5eb6a963b39c3bf6d9b633a0b9a14d
Alexander Fabisch <afabisch@googlemail.com>
2020-11-27 15:09:50 +0100

    Rescale axes

--- scripts/plot_relative_trajectories_kuka.py
+++ scripts/plot_relative_trajectories_kuka.py
@@ -41,8 +41,8 @@ def load_data(path):
 n_weights_per_dim = 10
 
 #pattern = "data/kuka/20200129_peg_in_hole/csv_processed/01_peg_in_hole_both_arms/*.csv"
-pattern = "data/kuka/20191213_carry_heavy_load/csv_processed/01_heavy_load_no_tilt_0cm_dual_arm/*.csv"
-#pattern = "data/kuka/20191023_rotate_panel_varying_size/csv_processed/panel_450mm_counterclockwise/*.csv"
+#pattern = "data/kuka/20191213_carry_heavy_load/csv_processed/01_heavy_load_no_tilt_0cm_dual_arm/*.csv"
+pattern = "data/kuka/20191023_rotate_panel_varying_size/csv_processed/panel_450mm_counterclockwise/*.csv"
 
 all_T = []
 all_P = []
@@ -81,13 +81,17 @@ all_parameters = np.vstack([
 mean_parameters = np.mean(all_parameters, axis=0)
 cov_parameters = np.cov(all_parameters, rowvar=False)
 
+all_T = np.asarray(all_T)
+all_P = np.asarray(all_P)
+
 plt.figure()
 axes = [plt.subplot(2, 3, d) for d in range(1, 7)]
 
 for T, P in zip(all_T, all_P):
-    axes[0].plot(T, P[:, 0] - P[:, 7], color="k", alpha=0.3)
-    axes[1].plot(T, P[:, 1] - P[:, 8], color="k", alpha=0.3)
-    axes[2].plot(T, P[:, 2] - P[:, 9], color="k", alpha=0.3)
+    dP = P[:, :3] - P[:, 7:10]
+    axes[0].plot(T, dP[:, 0], color="k", alpha=0.3)
+    axes[1].plot(T, dP[:, 1], color="k", alpha=0.3)
+    axes[2].plot(T, dP[:, 2], color="k", alpha=0.3)
 
 # next trajectories are sampled from Gaussian DMP
 random_state = np.random.RandomState(0)
@@ -111,5 +115,10 @@ for idx in range(5):
     axes[3].plot(T, P[:, 0] - P[:, 7], color="k")
     axes[4].plot(T, P[:, 1] - P[:, 8], color="k")
     axes[5].plot(T, P[:, 2] - P[:, 9], color="k")
+
+for d in range(3):
+    ylim = axes[d].get_ylim()
+    axes[d].set_ylim((ylim[0] - 0.05, ylim[1] + 0.05))
+    axes[3 + d].set_ylim((ylim[0] - 0.05, ylim[1] + 0.05))
 
 plt.show()
