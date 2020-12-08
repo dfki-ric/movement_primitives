7ec6e2a6cfaf08266cc8c83389bacdb4a5ec94a4
Alexander Fabisch <afabisch@googlemail.com>
2020-12-08 17:52:26 +0100

    Refactor example

--- examples/render_cartesian_dual_dmp.py
+++ examples/render_cartesian_dual_dmp.py
@@ -54,17 +54,12 @@ for i, t in enumerate(sigmoid):
     Y[i, 10:] = pr.quaternion_slerp(q_start, q_end, t)
 
 
-tm = UrdfTransformManager()
-with open("abstract-urdf-gripper/urdf/GripperLeft.urdf", "r") as f:
-    tm.load_urdf(f, mesh_path="abstract-urdf-gripper/urdf/")
-with open("abstract-urdf-gripper/urdf/GripperRight.urdf", "r") as f:
-    tm.load_urdf(f, mesh_path="abstract-urdf-gripper/urdf/")
-tm.add_transform("ALWristPitch_Link", "base", np.eye(4))
-tm.add_transform("ARWristPitch_Link", "base", np.eye(4))
-#assert tm.check_consistency()
-
-
-for coupling_term in [ct]:#[ct, None]:
+recorded_trajectories = []
+for coupling_term in [
+        CouplingTermDualCartesianPose(desired_distance=desired_distance, couple_position=True, couple_orientation=True, lf=(1.0, 0.0), k=1, c1=0.1, c2=10000, verbose=0),
+        #CouplingTermDualCartesianPose(desired_distance=desired_distance, couple_position=False, couple_orientation=True, lf=(1.0, 0.0), k=1, c1=0.1, c2=10000, verbose=0),
+        #CouplingTermDualCartesianPose(desired_distance=desired_distance, couple_position=True, couple_orientation=False, lf=(1.0, 0.0), k=1, c1=0.1, c2=10000, verbose=0),
+        None]:
     # TODO reset DMP properly
     dmp = DualCartesianDMP(
         execution_time=execution_time, dt=dt,
@@ -72,60 +67,45 @@ for coupling_term in [ct]:#[ct, None]:
     dmp.imitate(T, Y)
     dmp.configure(start_y=Y[0], goal_y=Y[-1])
 
-    #import time
-    #if coupling_term is not None:
-    #    time.sleep(20)
     rh5.goto_ee_state(Y[0])
     desired_positions, positions, desired_velocities, velocities = \
         rh5.step_through_cartesian(dmp, Y[0], np.zeros(12), execution_time, coupling_term=coupling_term)
     P = np.asarray(positions)
-    V = np.asarray(velocities)
 
-    fig = vis.figure()
-    fig.plot_basis(R=np.eye(3), s=0.1)
-    fig.plot_trajectory(P=P[:, :7], s=0.05, c=[1, 0.5, 0])
-    fig.plot_trajectory(P=P[:, 7:], s=0.05, c=[1, 0.5, 0])
-    tm.add_transform("ALWristPitch_Link", "base", np.eye(4))
-    tm.add_transform("ARWristPitch_Link", "base", np.eye(4))
-    graph = fig.plot_graph(tm, "base", show_visuals=True, show_frames=True, whitelist=["ALWristPitch_Link", "ARWristPitch_Link"], s=0.03)
-    def animation_callback(t, P, tm, graph):
-        gripper_left2base = pt.transform_from_pq(P[10 * t, :7])
-        gripper_right2base = pt.transform_from_pq(P[10 * t, 7:])
-        tm.add_transform("ALWristPitch_Link", "base", gripper_left2base)
-        tm.add_transform("ARWristPitch_Link", "base", gripper_right2base)
-        graph.set_data()
-        return graph
-    #ax.plot(P[:, 0], P[:, 1], P[:, 2], color="orange", lw=1)
-    #ax.plot(P[:, 7], P[:, 8], P[:, 9], color="orange", lw=1)
-    #ppu.plot_vector(
-    #    ax=ax, start=P[0, :3] + np.array([0, 0, 0.2]), direction=P[-1, :3] - P[0, :3],
-    #    lw=0, color="orange")
-    #ppu.plot_vector(
-    #    ax=ax, start=P[0, 7:10] - np.array([0, 0, 0.2]), direction=P[-1, 7:10] - P[0, 7:10],
-    #    lw=0, color="orange")
-    fig.view_init(elev=0, azim=90)
-    fig.animate(animation_callback, len(P) // 10, loop=True, fargs=(P, tm, graph))
-    fig.show()
+    recorded_trajectories.append(P)
 
-    """
-    dP = np.asarray(desired_positions)
-    dV = np.asarray(desired_velocities)
-    for subplot_idx, plot_dim in enumerate(range(14)):
-        plt.subplot(3, 7, 1 + subplot_idx)
-        plt.plot(T, Y[:, plot_dim], label="Demo %d" % plot_dim, c="k")
-        plt.scatter([[0, T[-1]]], [[Y[0, plot_dim], Y[-1, plot_dim]]], c="k")
-        plt.plot(np.linspace(0, execution_time, len(P)), P[:, plot_dim], label="Actual %d" % plot_dim, c="r")
-        plt.scatter([[0, execution_time]], [[P[0, plot_dim], P[-1, plot_dim]]], c="r")
-        plt.plot(np.linspace(0, execution_time, len(dP)), dP[:, plot_dim], label="Desired %d" % plot_dim, c="g", ls="--")
-        plt.scatter([[0, execution_time]], [[dP[0, plot_dim], dP[-1, plot_dim]]], c="g")
-        plt.ylim((min(P[:, plot_dim] - 0.05), max(P[:, plot_dim]) + 0.05))
-    for subplot_idx, plot_dim in enumerate(range(7)):
-        plt.subplot(3, 7, 15 + subplot_idx)
-        plt.plot(T, Y[:, plot_dim] - Y[:, 7 + plot_dim], label="Demo %d" % plot_dim, c="k")
-        plt.plot(np.linspace(0, execution_time, len(P)), P[:, plot_dim] - P[:, 7 + plot_dim], label="Actual %d" % plot_dim, c="r")
-        plt.plot(np.linspace(0, execution_time, len(dP)), dP[:, plot_dim] - dP[:, 7 + plot_dim], label="Desired %d" % plot_dim, c="g", ls="--")
-        D = P[:, plot_dim] - P[:, 7 + plot_dim]
-        plt.ylim((min(D - 0.05), max(D) + 0.05))
-    plt.legend()
-    plt.show()
-    """
+tm = UrdfTransformManager(check=False)
+with open("abstract-urdf-gripper/urdf/GripperLeft.urdf", "r") as f:
+    tm.load_urdf(f, mesh_path="abstract-urdf-gripper/urdf/")
+with open("abstract-urdf-gripper/urdf/GripperRight.urdf", "r") as f:
+    tm.load_urdf(f, mesh_path="abstract-urdf-gripper/urdf/")
+tm.add_transform("ALWristPitch_Link", "base", np.eye(4))
+tm.add_transform("ARWristPitch_Link", "base", np.eye(4))
+# assert tm.check_consistency()
+
+fig = vis.figure()
+fig.plot_basis(R=np.eye(3), s=0.1)
+for P, c in zip(recorded_trajectories, [[0, 1, 0], [1, 0, 0], [1, 0.5, 0], [0, 0, 0]]):
+    fig.plot_trajectory(P=P[:, :7], s=0.05, c=c)
+    fig.plot_trajectory(P=P[:, 7:], s=0.05, c=c)
+graph = fig.plot_graph(tm, "base", show_visuals=True, show_frames=True,
+                       whitelist=["ALWristPitch_Link", "ARWristPitch_Link"], s=0.03)
+
+
+def animation_callback(t, recorded_trajectories, tm, graph):
+    k = t // (len(recorded_trajectories[0]) // 10)
+    P = recorded_trajectories[k]
+    t = t % (len(recorded_trajectories[0]) // 10)
+    gripper_left2base = pt.transform_from_pq(P[10 * t, :7])
+    gripper_right2base = pt.transform_from_pq(P[10 * t, 7:])
+    tm.add_transform("ALWristPitch_Link", "base", gripper_left2base)
+    tm.add_transform("ARWristPitch_Link", "base", gripper_right2base)
+    graph.set_data()
+    return graph
+
+
+fig.view_init(elev=0, azim=90)
+fig.animate(
+    animation_callback, len(recorded_trajectories) * len(recorded_trajectories[0]) // 10,
+    loop=True, fargs=(recorded_trajectories, tm, graph))
+fig.show()
