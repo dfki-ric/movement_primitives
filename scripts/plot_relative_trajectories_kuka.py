8c77de468fae4b8f8da8c15416830ab3c38b6788
Alexander Fabisch <afabisch@googlemail.com>
2021-01-13 18:29:39 +0100

    Simplify data loading

--- scripts/plot_relative_trajectories_kuka.py
+++ scripts/plot_relative_trajectories_kuka.py
@@ -1,41 +1,8 @@
 import glob
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-from mocap.pandas_utils import match_columns, rename_stream_groups
-from mocap.conversion import array_from_dataframe
+from movement_primitives.data import load_kuka_dataset
 from movement_primitives.dmp import DualCartesianDMP
-
-
-def load_data(path):
-    trajectory = pd.read_csv(path, sep=" ")
-    patterns = ["time\.microseconds",
-                "kuka_lbr_cart_pos_ctrl_left\.current_feedback\.pose\.position\.data.*",
-                "kuka_lbr_cart_pos_ctrl_left\.current_feedback\.pose\.orientation\.re.*",
-                "kuka_lbr_cart_pos_ctrl_left\.current_feedback\.pose\.orientation\.im.*",
-                "kuka_lbr_cart_pos_ctrl_right\.current_feedback\.pose\.position\.data.*",
-                "kuka_lbr_cart_pos_ctrl_right\.current_feedback\.pose\.orientation\.re.*",
-                "kuka_lbr_cart_pos_ctrl_right\.current_feedback\.pose\.orientation\.im.*"]
-    columns = match_columns(trajectory, patterns)
-    trajectory = trajectory[columns]
-    group_rename = {
-        "(time\.microseconds)": "Time",
-        "(kuka_lbr_cart_pos_ctrl_left\.current_feedback\.pose\.position\.data).*": "left_pose",
-        "(kuka_lbr_cart_pos_ctrl_left\.current_feedback\.pose\.orientation).*": "left_pose",
-        "(kuka_lbr_cart_pos_ctrl_right\.current_feedback\.pose\.position\.data).*": "right_pose",
-        "(kuka_lbr_cart_pos_ctrl_right\.current_feedback\.pose\.orientation).*": "right_pose"
-    }
-    trajectory = rename_stream_groups(trajectory, group_rename)
-    trajectory["Time"] = trajectory["Time"] / 1e6
-    trajectory["Time"] -= trajectory["Time"].iloc[0]
-    T = trajectory["Time"].to_numpy()
-    P = array_from_dataframe(
-        trajectory,
-        ["left_pose[0]", "left_pose[1]", "left_pose[2]",
-        "left_pose.re", "left_pose.im[0]", "left_pose.im[1]", "left_pose.im[2]",
-        "right_pose[0]", "right_pose[1]", "right_pose[2]",
-        "right_pose.re", "right_pose.im[0]", "right_pose.im[1]", "right_pose.im[2]"])
-    return T, P
 
 
 n_weights_per_dim = 10
@@ -44,17 +11,15 @@ n_weights_per_dim = 10
 #pattern = "data/kuka/20191213_carry_heavy_load/csv_processed/01_heavy_load_no_tilt_0cm_dual_arm/*.csv"
 pattern = "data/kuka/20191023_rotate_panel_varying_size/csv_processed/panel_450mm_counterclockwise/*.csv"
 
+dataset = load_kuka_dataset(pattern, verbose=1)
+
 all_T = []
 all_P = []
 all_weights = []
 all_starts = []
 all_goals = []
 all_execution_times = []
-
-for idx, path in enumerate(list(glob.glob(pattern))):
-    print("Loading %s" % path)
-    T, P = load_data(path)
-
+for T, P in dataset:
     execution_time = T[-1]
     dt = np.mean(np.diff(T))
     if dt < 0.005:  # HACK
