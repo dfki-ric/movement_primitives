0b4e9bbbf0230313b71e86d5e544bb07d09bf632
Alexander Fabisch <afabisch@googlemail.com>
2021-01-26 17:20:17 +0100

    Move promp example to tests

--- test/test_promp.py
+++ test/test_promp.py
@@ -3,6 +3,31 @@ from movement_primitives.promp import ProMP
 from nose.tools import assert_less
 
 
+def test_imitate():
+    n_weights_per_dim = 50
+
+    promp = ProMP(n_dims=1, n_weights_per_dim=n_weights_per_dim)
+
+    random_state = np.random.RandomState(10)
+    n_demos = 10
+    n_steps = 101
+    T = np.empty((n_demos, n_steps))
+    T[:, :] = np.linspace(0.0, 1.0, n_steps)
+    Y = np.empty((n_demos, n_steps, 1))
+    for demo_idx in range(n_demos):
+        Y[demo_idx] = np.cos(2 * np.pi * T[demo_idx] + random_state.randn() * 0.1)[:, np.newaxis]
+        Y[demo_idx, :, 0] += random_state.randn(n_steps) * 0.01
+    promp.imitate(T, Y)
+
+    random_state = np.random.RandomState(0)
+    samples = promp.sample_trajectories(T[0], 10, random_state)
+
+    mean_trajectory = promp.mean_trajectory(T[0])
+    assert_less(np.linalg.norm(np.mean(samples, axis=0) - mean_trajectory) / n_steps, 0.001)
+    std_trajectory = np.sqrt(promp.var_trajectory(T[0]))
+    assert_less(np.linalg.norm(np.std(samples, axis=0) - std_trajectory) / n_steps, 0.001)
+
+
 def test_promp():
     n_weights_per_dim = 15
 
@@ -33,6 +58,42 @@ def test_promp():
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
+
+    plt.figure()
+    n_weights_per_dim = 50
+
+    promp = ProMP(n_dims=1, n_weights_per_dim=n_weights_per_dim)
+
+    random_state = np.random.RandomState(10)
+    n_demos = 10
+    n_steps = 101
+    T = np.empty((n_demos, n_steps))
+    T[:, :] = np.linspace(0.0, 1.0, n_steps)
+    Y = np.empty((n_demos, n_steps, 1))
+    for demo_idx in range(n_demos):
+        Y[demo_idx] = np.cos(2 * np.pi * T[demo_idx] + random_state.randn() * 0.1)[:, np.newaxis]
+        Y[demo_idx, :, 0] += random_state.randn(n_steps) * 0.01
+    promp.imitate(T, Y, verbose=1)
+
+    for demo_idx in range(n_demos):
+        plt.plot(T[demo_idx, :], Y[demo_idx, :], c="k", alpha=0.1)
+
+    random_state = np.random.RandomState(0)
+    samples = promp.sample_trajectories(T[0], 10, random_state)
+
+    mean_trajectory = promp.mean_trajectory(T[0])
+    plt.plot(T[0], mean_trajectory, label="Reproduction", c="r", lw=3)
+    var_trajectory = np.sqrt(promp.var_trajectory(T[0]))
+    factor = 2
+    plt.fill_between(
+        T[0],
+        mean_trajectory[:, 0] - factor * var_trajectory[:, 0],
+        mean_trajectory[:, 0] + factor * var_trajectory[:, 0],
+        alpha=0.3)
+    for sample in samples:
+        plt.plot(T[0], sample, c="g", alpha=0.3)
+
+    plt.figure()
 
     n_weights_per_dim = 15
 
