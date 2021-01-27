cccdf2da8f05b3aa3ed6f002244939872d4ec982
Alexander Fabisch <afabisch@googlemail.com>
2021-01-27 10:21:17 +0100

    Switch to new URDF and turn panel

--- scripts/vis_solar_panel.py
+++ scripts/vis_solar_panel.py
@@ -14,7 +14,7 @@ def panel_pose(tm):
     tcp_right_pos = tcp_right[:3, 3]
     tcp_middle = 0.5 * (tcp_left_pos + tcp_right_pos)
     x_axis = pr.norm_vector(tcp_right_pos - tcp_left_pos)
-    y_axis = pr.norm_vector(0.5 * (tcp_left[:3, 1] + tcp_right[:3, 1]))
+    y_axis = -pr.norm_vector(0.5 * (tcp_left[:3, 1] + tcp_right[:3, 1]))
     R_panel = pr.matrix_from_two_vectors(x_axis, y_axis)
     return pt.transform_from(R_panel, tcp_middle)
 
@@ -31,8 +31,8 @@ solar_panel_idx = 0
 panel_rotation_angle = np.deg2rad(90)
 n_steps = 51
 
-with open("abstract-urdf-gripper/urdf/rh5_fixed.urdf", "r") as f:
-    kin = Kinematics(f.read(), mesh_path="abstract-urdf-gripper/urdf/")
+with open("pybullet-only-arms-urdf/urdf/RH5.urdf", "r") as f:
+    kin = Kinematics(f.read(), mesh_path="pybullet-only-arms-urdf/urdf/")
 left_arm = kin.create_chain(
     ["ALShoulder1", "ALShoulder2", "ALShoulder3",
      "ALElbow", "ALWristRoll", "ALWristYaw", "ALWristPitch"],
@@ -60,7 +60,7 @@ left2panel_start = pt.concat(left2base_start, pt.invert_transform(panel2base_sta
 right2panel_start = pt.concat(right2base_start, pt.invert_transform(panel2base_start))
 
 
-rotation_axis = pr.unity
+rotation_axis = -pr.unity
 start2end = pt.rotate_transform(np.eye(4), pr.matrix_from_compact_axis_angle(rotation_axis * panel_rotation_angle))
 left2panel_end = pt.concat(left2panel_start, start2end)
 right2panel_end = pt.concat(right2panel_start, start2end)
