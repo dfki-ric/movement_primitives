851db0ad547ab69ad245ae470d6bb661d1c5ba85
Alexander Fabisch <afabisch@googlemail.com>
2021-01-28 17:43:59 +0100

    Refactor script

--- scripts/vis_sample_dmp_distribution_kuka.py
+++ scripts/vis_sample_dmp_distribution_kuka.py
@@ -1,56 +1,9 @@
-import glob
 import numpy as np
-import pandas as pd
-from mocap.pandas_utils import match_columns, rename_stream_groups
-from mocap.conversion import array_from_dataframe
+from movement_primitives.data import load_kuka_dataset
 import pytransform3d.visualizer as pv
 from pytransform3d.urdf import UrdfTransformManager
 from movement_primitives.dmp import DualCartesianDMP
 
-
-def load_data(path):
-    trajectory = pd.read_csv(path, sep=" ")
-    patterns = ["time\.microseconds",
-                "kuka_lbr_cart_pos_ctrl_left\.current_feedback\.pose\.position\.data.*",
-                "kuka_lbr_cart_pos_ctrl_left\.current_feedback\.pose\.orientation\.re.*",
-                "kuka_lbr_cart_pos_ctrl_left\.current_feedback\.pose\.orientation\.im.*",
-                "kuka_lbr_cart_pos_ctrl_right\.current_feedback\.pose\.position\.data.*",
-                "kuka_lbr_cart_pos_ctrl_right\.current_feedback\.pose\.orientation\.re.*",
-                "kuka_lbr_cart_pos_ctrl_right\.current_feedback\.pose\.orientation\.im.*"]
-    columns = match_columns(trajectory, patterns)
-    trajectory = trajectory[columns]
-    group_rename = {
-        "(time\.microseconds)": "Time",
-        "(kuka_lbr_cart_pos_ctrl_left\.current_feedback\.pose\.position\.data).*": "left_pose",
-        "(kuka_lbr_cart_pos_ctrl_left\.current_feedback\.pose\.orientation).*": "left_pose",
-        "(kuka_lbr_cart_pos_ctrl_right\.current_feedback\.pose\.position\.data).*": "right_pose",
-        "(kuka_lbr_cart_pos_ctrl_right\.current_feedback\.pose\.orientation).*": "right_pose"
-    }
-    trajectory = rename_stream_groups(trajectory, group_rename)
-    trajectory["Time"] = trajectory["Time"] / 1e6
-    trajectory["Time"] -= trajectory["Time"].iloc[0]
-    T = trajectory["Time"].to_numpy()
-    P = array_from_dataframe(
-        trajectory,
-        ["left_pose[0]", "left_pose[1]", "left_pose[2]",
-        "left_pose.re", "left_pose.im[0]", "left_pose.im[1]", "left_pose.im[2]",
-        "right_pose[0]", "right_pose[1]", "right_pose[2]",
-        "right_pose.re", "right_pose.im[0]", "right_pose.im[1]", "right_pose.im[2]"])
-    return T, P
-
-
-fig = pv.figure(with_key_callbacks=True)
-fig.plot_transform(s=0.1)
-tm = UrdfTransformManager()
-with open("kuka_lbr/urdf/kuka_lbr.urdf", "r") as f:
-    tm.load_urdf(f.read(), mesh_path="kuka_lbr/urdf/")
-fig.plot_graph(tm, "kuka_lbr", show_visuals=True)
-
-n_weights_per_dim = 10
-
-#pattern = "data/kuka/20200129_peg_in_hole/csv_processed/01_peg_in_hole_both_arms/*.csv"
-pattern = "data/kuka/20191213_carry_heavy_load/csv_processed/01_heavy_load_no_tilt_0cm_dual_arm/*.csv"
-#pattern = "data/kuka/20191023_rotate_panel_varying_size/csv_processed/panel_450mm_counterclockwise/*.csv"
 
 class SelectDemo:
     def __init__(self, fig, left, right):
@@ -70,15 +23,30 @@ class SelectDemo:
         fig.view_init(azim=0, elev=25)
         return True
 
+
+fig = pv.figure(with_key_callbacks=True)
+fig.plot_transform(s=0.1)
+tm = UrdfTransformManager()
+with open("kuka_lbr/urdf/kuka_lbr.urdf", "r") as f:
+    tm.load_urdf(f.read(), mesh_path="kuka_lbr/urdf/")
+fig.plot_graph(tm, "kuka_lbr", show_visuals=True)
+
+n_weights_per_dim = 10
+
+#pattern = "data/kuka/20200129_peg_in_hole/csv_processed/01_peg_in_hole_both_arms/*.csv"
+pattern = "data/kuka/20191213_carry_heavy_load/csv_processed/01_heavy_load_no_tilt_0cm_dual_arm/*.csv"
+#pattern = "data/kuka/20191023_rotate_panel_varying_size/csv_processed/panel_450mm_counterclockwise/*.csv"
+
+dataset = load_kuka_dataset(pattern, verbose=1)
+
 all_weights = []
 all_starts = []
 all_goals = []
 all_execution_times = []
 
 # first N trajectories are from training set
-for idx, path in enumerate(list(glob.glob(pattern))[:10]):
-    print("Loading %s" % path)
-    T, P = load_data(path)
+for idx in range(5):
+    T, P = dataset[idx]
 
     execution_time = T[-1]
     dt = np.mean(np.diff(T))
