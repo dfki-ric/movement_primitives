ba642fe7024935f5d63cd8032b34c001bf6b799b
Alexander Fabisch <alexander.fabisch@dfki.de>
2021-08-20 17:17:39 +0200

    Test stepping through spring-damper system

--- test/test_spring_damper.py
+++ test/test_spring_damper.py
@@ -3,6 +3,7 @@ from pytransform3d import rotations as pr
 from pytransform3d import trajectories as ptr
 from movement_primitives.spring_damper import SpringDamper, SpringDamperOrientation
 from numpy.testing import assert_array_almost_equal
+from nose.tools import assert_less
 
 
 def test_spring_damper():
@@ -18,6 +19,23 @@ def test_spring_damper():
             assert_array_almost_equal(Y[-1], goal_y, decimal=2)
 
 
+def test_spring_damper_steps():
+    start_y = np.array([0.0])
+    start_yd = np.array([0.0])
+    goal_y = np.array([1.0])
+
+    k = 100.0
+    c = 2.0 * np.sqrt(k)
+    sd = SpringDamper(n_dims=1, dt=0.01, k=k, c=c, int_dt=0.001)
+    sd.configure(start_y=start_y, goal_y=goal_y, start_yd=start_yd)
+    y = np.copy(start_y)
+    yd = np.copy(start_yd)
+    for i in range(100):
+        y, yd = sd.step(y, yd)
+    error = np.linalg.norm(goal_y - y)
+    assert_less(error, 6e-4)
+
+
 def test_spring_damper_quaternion():
     dt = 0.01
     execution_time = 5.0
@@ -31,6 +49,24 @@ def test_spring_damper_quaternion():
     T, Q = sd.open_loop(run_t=execution_time)
     assert_array_almost_equal(Q[0], start, decimal=3)
     assert_array_almost_equal(Q[-1], goal, decimal=3)
+
+
+def test_spring_damper_quaternion_steps():
+    random_state = np.random.RandomState(42)
+    start = pr.random_quaternion(random_state)
+    start_d = np.array([0.0, 0.0, 0.0])
+    goal = np.array([0.0, 0.0, 1.0, 0.0])
+
+    k = 100.0
+    c = 2.0 * np.sqrt(k)
+    sd = SpringDamperOrientation(dt=0.01, k=k, c=c, int_dt=0.001)
+    sd.configure(start_y=start, goal_y=goal, start_yd=start_d)
+    y = np.copy(start)
+    yd = np.copy(start_d)
+    for i in range(100):
+        y, yd = sd.step(y, yd)
+    error = np.linalg.norm(goal - y)
+    assert_less(error, 1e-3)
 
 
 if __name__ == "__main__":
