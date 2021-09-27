5367bbbe1011a5a5aca1e9e8d29a9d1faab20a87
Alexander Fabisch <afabisch@googlemail.com>
2021-09-27 16:30:42 +0200

    Refactor example

--- examples/plot_promp_lasa.py
+++ examples/plot_promp_lasa.py
@@ -17,34 +17,35 @@ from movement_primitives.data import load_lasa
 from movement_primitives.promp import ProMP
 
 
-def draw(T, X, promp, idx, axs):
+def draw(T, X, idx, axes, shape_name):
     h = int(idx / width)
     w = int(idx % width) * 2
-    axs[h, w].plot(X[:, :, 0].T, X[:, :, 1].T)
+    axes[h, w].set_title(shape_name)
+    axes[h, w].plot(X[:, :, 0].T, X[:, :, 1].T)
 
+    promp = ProMP(n_weights_per_dim=30, n_dims=X.shape[2])
+    promp.imitate(T, X)
     mean = promp.mean_trajectory(T[0])
     std = np.sqrt(promp.var_trajectory(T[0]))
-    axs[h, w + 1].plot(mean[:, 0], mean[:, 1], c="r")
-    axs[h, w + 1].plot(mean[:, 0] - std[:, 0], mean[:, 1] - std[:, 1], c="g")
-    axs[h, w + 1].plot(mean[:, 0] + std[:, 0], mean[:, 1] + std[:, 1], c="g")
+    axes[h, w + 1].plot(mean[:, 0], mean[:, 1], c="r")
+    axes[h, w + 1].plot(mean[:, 0] - std[:, 0], mean[:, 1] - std[:, 1], c="g")
+    axes[h, w + 1].plot(mean[:, 0] + std[:, 0], mean[:, 1] + std[:, 1], c="g")
 
-    axs[h, w + 1].set_xlim(axs[h, w].get_xlim())
-    axs[h, w + 1].set_ylim(axs[h, w].get_ylim())
-    axs[h, w].get_yaxis().set_visible(False)
-    axs[h, w].get_xaxis().set_visible(False)
-    axs[h, w + 1].get_yaxis().set_visible(False)
-    axs[h, w + 1].get_xaxis().set_visible(False)
+    axes[h, w + 1].set_xlim(axes[h, w].get_xlim())
+    axes[h, w + 1].set_ylim(axes[h, w].get_ylim())
+    axes[h, w].get_yaxis().set_visible(False)
+    axes[h, w].get_xaxis().set_visible(False)
+    axes[h, w + 1].get_yaxis().set_visible(False)
+    axes[h, w + 1].get_xaxis().set_visible(False)
 
 
-n_shapes = 10
 width = 2
 height = 5
 
 fig, axes = plt.subplots(int(height), int(width * 2))
 
-for i in range(n_shapes):
+for i in range(width * height):
     T, X, Xd, Xdd, dt, shape_name = load_lasa(i)
-    promp = ProMP(n_weights_per_dim=30, n_dims=2)
-    promp.imitate(T, X)
-    draw(T, X, promp, i, axes)
+    draw(T, X, i, axes, shape_name)
+plt.tight_layout()
 plt.show()
