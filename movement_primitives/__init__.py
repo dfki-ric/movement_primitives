2b6a36788e135df216e86db29780222dca4f740f
Alexander Fabisch <afabisch@googlemail.com>
2021-11-01 15:57:51 +0100

    Add docstring for main module

--- movement_primitives/__init__.py
+++ movement_primitives/__init__.py
@@ -1,4 +1,10 @@
-"""Movement primitives."""
+"""Movement primitives.
+
+Movement primitives are a common policy representation in robotics. There are
+many different types and variations. This library focuses mainly on
+imitation learning, generalization, and adaptation of movement primitives and
+provides implementations in Python and Cython.
+"""
 
 
 __version__ = "0.0.1-0"
