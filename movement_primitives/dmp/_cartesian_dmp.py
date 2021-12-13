f262c6010fecb6bba536e7bf0375e2dfe074d7fa
Alexander Fabisch <alexander.fabisch@dfki.de>
2021-12-13 11:12:20 +0100

    Test and fix get_weights

--- movement_primitives/dmp/_cartesian_dmp.py
+++ movement_primitives/dmp/_cartesian_dmp.py
@@ -195,8 +195,8 @@ class CartesianDMP(DMPBase):
         weights : array, shape (6 * n_weights_per_dim,)
             Current weights of the DMP.
         """
-        return np.concatenate(self.forcing_term_pos.weights.ravel(),
-                              self.forcing_term_rot.weights.ravel())
+        return np.concatenate((self.forcing_term_pos.weights.ravel(),
+                               self.forcing_term_rot.weights.ravel()))
 
     def set_weights(self, weights):
         """Set weight vector of DMP.
--- test/test_cartesian_dmp.py
+++ test/test_cartesian_dmp.py
@@ -68,3 +68,38 @@ def test_compare_python_cython():
 
     assert_array_almost_equal(kwargs_python["current_y"], kwargs_cython["current_y"])
     assert_array_almost_equal(kwargs_python["current_yd"], kwargs_cython["current_yd"])
+
+
+def test_get_set_weights():
+    dt = 0.001
+    execution_time = 1.0
+
+    dmp = CartesianDMP(
+        execution_time=execution_time, dt=dt,
+        n_weights_per_dim=10, int_dt=0.0001)
+    Y = np.zeros((1001, 7))
+    T = np.linspace(0, 1, len(Y))
+    sigmoid = 0.5 * (np.tanh(1.5 * np.pi * (T - 0.5)) + 1.0)
+    Y[:, 0] = 0.6
+    Y[:, 1] = -0.2 + 0.4 * sigmoid
+    Y[:, 2] = 0.45
+    start_aa = np.array([0.0, 1.0, 0.0, 0.25 * np.pi])
+    goal_aa = np.array([0.0, 0.0, 1.0, 0.25 * np.pi])
+    for t in range(len(Y)):
+        frac = sigmoid[t]
+        aa_t = (1.0 - frac) * start_aa + frac * goal_aa
+        aa_t[:3] /= np.linalg.norm(aa_t[:3])
+        Y[t, 3:] = pr.quaternion_from_axis_angle(aa_t)
+    dmp.imitate(T, Y, allow_final_velocity=True)
+    dmp.configure(start_y=Y[0], goal_y=Y[-1])
+
+    T2, Y2 = dmp.open_loop()
+    assert_array_almost_equal(T, T2)
+    assert_array_almost_equal(Y, Y2, decimal=2)
+
+    weights = dmp.get_weights()
+    dmp.set_weights(weights)
+
+    T3, Y3 = dmp.open_loop()
+    assert_array_almost_equal(T, T3)
+    assert_array_almost_equal(Y, Y3, decimal=2)
