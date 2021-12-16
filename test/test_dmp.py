ee490d8d3f20e557d40efbe053e7247a8e232aad
Alexander Fabisch <afabisch@googlemail.com>
2021-12-16 12:19:32 +0100

    Test getter and setter for weights in DMP

--- test/test_dmp.py
+++ test/test_dmp.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pytransform3d import rotations as pr
 from movement_primitives.dmp import DMP
 from nose.tools import (assert_almost_equal, assert_equal, assert_less,
                         assert_raises_regexp)
@@ -160,6 +161,41 @@ def test_set_current_time():
         y, yd = dmp.step(y, yd)
     error = np.linalg.norm(goal_y - y)
     assert_less(error, 1e-2)
+
+
+def test_get_set_weights():
+    dt = 0.001
+    execution_time = 1.0
+
+    dmp = DMP(
+        execution_time=execution_time, n_dims=7, dt=dt,
+        n_weights_per_dim=10, int_dt=0.0001)
+    Y = np.zeros((1001, 7))
+    T = np.linspace(0, 1, len(Y))
+    sigmoid = 0.5 * (np.tanh(1.5 * np.pi * (T - 0.5)) + 1.0)
+    Y[:, 0] = 0.6
+    Y[:, 1] = -0.2 + 0.4 * sigmoid
+    Y[:, 2] = 0.45
+    start_aa = np.array([0.0, 1.0, 0.0, 0.25 * np.pi])
+    goal_aa = np.array([0.0, 0.0, 1.0, 0.25 * np.pi])
+    for t in range(len(Y)):
+        frac = sigmoid[t]
+        aa_t = (1.0 - frac) * start_aa + frac * goal_aa
+        aa_t[:3] /= np.linalg.norm(aa_t[:3])
+        Y[t, 3:] = pr.quaternion_from_axis_angle(aa_t)
+    dmp.imitate(T, Y, allow_final_velocity=True)
+    dmp.configure(start_y=Y[0], goal_y=Y[-1])
+
+    T2, Y2 = dmp.open_loop()
+    assert_array_almost_equal(T, T2)
+    assert_array_almost_equal(Y, Y2, decimal=2)
+
+    weights = dmp.get_weights()
+    dmp.set_weights(weights)
+
+    T3, Y3 = dmp.open_loop()
+    assert_array_almost_equal(T, T3)
+    assert_array_almost_equal(Y, Y3, decimal=2)
 
 
 if __name__ == "__main__":
