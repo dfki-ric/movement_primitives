fc6cf28efd36d67fcdd6d5f29a424431e79452a7
Alexander Fabisch <afabisch@googlemail.com>
2021-12-19 20:10:10 +0100

    Add missing docstrings

--- movement_primitives/dmp/_dmp.py
+++ movement_primitives/dmp/_dmp.py
@@ -196,7 +196,72 @@ def dmp_step_rk4(
         start_yd, start_ydd, goal_t, start_t, alpha_y, beta_y, forcing_term,
         coupling_term=None, coupling_term_precomputed=None, int_dt=0.001,
         p_gain=0.0, tracking_error=0.0):
-    """Integrate regular DMP for one step with RK4 integration."""
+    """Integrate regular DMP for one step with RK4 integration.
+
+    Parameters
+    ----------
+    last_t : float
+        Time at last step.
+
+    t : float
+        Time at current step.
+
+    current_y : array, shape (n_dims,)
+        Current position. Will be modified.
+
+    current_yd : array, shape (n_dims,)
+        Current velocity. Will be modified.
+
+    goal_y : array, shape (n_dims,)
+        Goal position.
+
+    goal_yd : array, shape (n_dims,)
+        Goal velocity.
+
+    goal_ydd : array, shape (n_dims,)
+        Goal acceleration.
+
+    start_y : array, shape (n_dims,)
+        Start position.
+
+    start_yd : array, shape (n_dims,)
+        Start velocity.
+
+    start_ydd : array, shape (n_dims,)
+        Start acceleration.
+
+    goal_t : float
+        Time at the end.
+
+    start_t : float
+        Time at the start.
+
+    alpha_y : float
+        Constant in transformation system.
+
+    beta_y : float
+        Constant in transformation system.
+
+    forcing_term : ForcingTerm
+        Forcing term.
+
+    coupling_term : CouplingTerm, optional (default: None)
+        Coupling term. Must have a function coupling(y, yd) that returns
+        additional velocity and acceleration.
+
+    coupling_term_precomputed : tuple
+        A precomputed coupling term, i.e., additional velocity and
+        acceleration.
+
+    int_dt : float, optional (default: 0.001)
+        Time delta used internally for integration.
+
+    p_gain : float, optional (default: 0)
+        Proportional gain for tracking error.
+
+    tracking_error : float, optional (default: 0)
+        Tracking error from last step.
+    """
     if coupling_term is None:
         cd, cdd = np.zeros_like(current_y), np.zeros_like(current_y)
         if coupling_term_precomputed is not None:
@@ -562,10 +627,32 @@ def dmp_imitate(
             start_y, start_yd, start_ydd, goal_y, goal_yd, goal_ydd)
 
 
-def ridge_regression(X, F, regularization_coefficient):  # returns: n_dims x n_weights_per_dim
+def ridge_regression(X, Y, regularization_coefficient):
+    r"""Ridge regression: linear regression with Tikhonov regularization.
+
+    A linear model :math:`Y = X W^T` or :math:`y = W x` that minimizes
+    :math:`||Y - X W^T||_2^2 + \lambda ||W||_2^2`, where :math:`\lambda`
+    is the regularization coefficient.
+
+    Parameters
+    ----------
+    X : array, shape (n_samples, n_features)
+        Input features of linear model.
+
+    Y : array, shape (n_samples, n_outputs)
+        Target values.
+
+    regularization_coefficient : float
+        Regularization coefficient.
+
+    Returns
+    -------
+    weights : array, shape (n_outputs, n_features)
+        Weights of linear model.
+    """
     return np.linalg.pinv(
         X.dot(X.T) + regularization_coefficient * np.eye(X.shape[0])
-    ).dot(X).dot(F).T
+    ).dot(X).dot(Y).T
 
 
 def dmp_open_loop(
