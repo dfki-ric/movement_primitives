285d6bc558b1aaa047078fe6eb33b0ea010437a0
Alexander Fabisch <afabisch@googlemail.com>
2023-05-22 15:09:04 +0200

    Test scaling

--- test/test_dmp.py
+++ test/test_dmp.py
@@ -225,6 +225,32 @@ def test_invalid_step_function():
                         step_function="invalid")
 
 
+def test_temporal_scaling():
+    execution_time = 2.0
+    dt = 0.001
+
+    dmp = DMP(n_dims=2, execution_time=execution_time, dt=dt,
+              n_weights_per_dim=100)
+
+    T = np.arange(0.0, execution_time + dt, dt)
+    Y_demo = np.empty((len(T), 2))
+    Y_demo[:, 0] = np.cos(2.5 * np.pi * T)
+    Y_demo[:, 1] = 0.5 + np.cos(1.5 * np.pi * T)
+    dmp.imitate(T, Y_demo)
+
+    dmp.configure(start_y=Y_demo[0], goal_y=Y_demo[-1])
+    _, Y2 = dmp.open_loop()
+
+    dmp.execution_time_ = 1.0
+    _, Y1 = dmp.open_loop()
+
+    dmp.execution_time_ = 4.0
+    _, Y4 = dmp.open_loop()
+
+    assert_less(np.linalg.norm(Y1 - Y2[::2]) / len(Y1), 1e-3)
+    assert_less(np.linalg.norm(Y2 - Y4[::2]) / len(Y2), 1e-3)
+
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     start_y = np.array([0.0])
