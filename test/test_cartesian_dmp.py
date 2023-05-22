327b192608747e20e555f59671903854045c4713
Alexander Fabisch <afabisch@googlemail.com>
2023-05-22 15:10:47 +0200

    Test scaling of Cartesian DMP

--- test/test_cartesian_dmp.py
+++ test/test_cartesian_dmp.py
@@ -2,7 +2,7 @@ import numpy as np
 from movement_primitives.dmp import CartesianDMP
 from pytransform3d import rotations as pr
 from numpy.testing import assert_array_almost_equal, assert_raises_regex
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_less
 
 
 def test_imitate_cartesian_dmp():
@@ -126,3 +126,38 @@ def test_invalid_step_function():
                         step_function="invalid")
     assert_raises_regex(ValueError, "Step function", dmp.open_loop,
                         quaternion_step_function="invalid")
+
+
+def test_temporal_scaling():
+    execution_time = 2.0
+    dt = 0.01
+
+    dmp = CartesianDMP(
+        execution_time=execution_time, dt=dt,
+        n_weights_per_dim=10, int_dt=0.0001)
+    Y = np.zeros((1001, 7))
+    T = np.linspace(0, 1, len(Y))
+    sigmoid = 0.5 * (np.tanh(1.5 * np.pi * (T - 0.5)) + 1.0)
+    Y[:, 0] = 0.6
+    Y[:, 1] = -0.2 + 0.4 * sigmoid
+    Y[:, 2] = 0.45
+    start_aa = np.array([0.0, 1.0, 0.0, 0.25 * np.pi])
+    goal_aa = np.array([0.0, 0.0, 1.0, 0.25 * np.pi])
+    for t in range(len(Y)):
+        frac = sigmoid[t]
+        aa_t = (1.0 - frac) * start_aa + frac * goal_aa
+        aa_t[:3] /= np.linalg.norm(aa_t[:3])
+        Y[t, 3:] = pr.quaternion_from_axis_angle(aa_t)
+    dmp.imitate(T, Y, allow_final_velocity=True)
+    dmp.configure(start_y=Y[0], goal_y=Y[-1])
+
+    _, Y2 = dmp.open_loop()
+
+    dmp.execution_time_ = 1.0
+    _, Y1 = dmp.open_loop()
+
+    dmp.execution_time_ = 4.0
+    _, Y4 = dmp.open_loop()
+
+    assert_less(np.linalg.norm(Y1 - Y2[::2]) / len(Y1), 1e-3)
+    assert_less(np.linalg.norm(Y2 - Y4[::2]) / len(Y2), 1e-3)
