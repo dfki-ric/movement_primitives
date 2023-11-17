Fix spatial scaling (in Python)

examples/plot_dmp_scaling.py
@@ -8,32 +8,42 @@ when the original demonstration has both the start and the goal state close
 together: small perturbation in the start and/or goal state often result in
 large accelerations. '''To fix this issue, our implementation of the DMP does
 not scale the forcing term by $(g - y_0)$!''' This example demonstrates the
-behavior of this DMP implementation in these cases.
+behavior of this DMP implementation in these cases. We adopted the modification
+of
+
+P. Pastor, H. Hoffmann, T. Asfour, S. Schaal:
+Learning and Generalization of Motor Skills by Learning from Demonstration
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from movement_primitives.dmp import DMP
+from movement_primitives.dmp import DMP, CartesianDMP, DualCartesianDMP
 
 
 T = np.linspace(0, 1, 101)
 x = np.sin(T ** 2 * 1.99 * np.pi)
 y = np.cos(T ** 2 * 1.99 * np.pi)
-Y = np.column_stack((x, y))
+z = qx = qy = qz = np.zeros_like(x)
+qw = np.ones_like(x)
+Y = np.column_stack((x, y, z, qw, qx, qy, qz, x, y, z, qw, qx, qy, qz))
 start = Y[0]
 goal = Y[-1]
-new_goal = np.array([0.5, 0.5])
+new_start = np.array([0.5, 0.5, 0, 1, 0, 0, 0, 0.5, 0.5, 0, 1, 0, 0, 0])
+new_goal = np.array([-0.5, 0.5, 0, 1, 0, 0, 0, -0.5, 0.5, 0, 1, 0, 0, 0])
 Y_shifted = Y - goal[np.newaxis] + new_goal[np.newaxis]
 
 dmp = DMP(n_dims=len(start), execution_time=1.0, dt=0.01, n_weights_per_dim=20)
+#dmp = CartesianDMP(execution_time=1.0, dt=0.01, n_weights_per_dim=20)
+#dmp = DualCartesianDMP(execution_time=1.0, dt=0.01, n_weights_per_dim=20)
 dmp.imitate(T, Y)
-dmp.configure(goal_y=new_goal)
-_, Y_dmp = dmp.open_loop()
+dmp.configure(start_y=new_start, goal_y=new_goal)
+_, Y_dmp = dmp.open_loop(step_function="euler")
 
 plt.plot(Y[:, 0], Y[:, 1], label=r"Demonstration, $g \approx y_0$", ls="--")
 plt.plot(Y_shifted[:, 0], Y_shifted[:, 1], label="Original shape with new goal", ls="--")
 plt.plot(Y_dmp[:, 0], Y_dmp[:, 1], label="DMP with new goal", lw=5, alpha=0.5)
 plt.scatter(start[0], start[1], c="r", label="Goal of demonstration: $g$")
 plt.scatter(goal[0], goal[1], c="g", label="Start of demonstration: $y_0$")
+plt.scatter(new_start[0], new_start[1], c="c", label="New start: $y_0'$")
 plt.scatter(new_goal[0], new_goal[1], c="y", label="New goal: $g'$")
 plt.xlabel("$y_1$")
 plt.ylabel("$y_2$")

movement_primitives/dmp/_cartesian_dmp.py
@@ -4,7 +4,7 @@ from ._base import DMPBase
 from ._forcing_term import ForcingTerm
 from ._canonical_system import canonical_system_alpha
 from ._dmp import (dmp_open_loop, dmp_imitate, ridge_regression,
-                   DMP_STEP_FUNCTIONS, DEFAULT_DMP_STEP_FUNCTION)
+                   DMP_STEP_FUNCTIONS, DEFAULT_DMP_STEP_FUNCTION, phase)
 
 
 def dmp_step_quaternion_python(
@@ -109,12 +109,21 @@ def dmp_step_quaternion_python(
 
         f = forcing_term(current_t).squeeze()
 
+        s = phase(current_t, forcing_term.alpha_z, goal_t, start_t, int_dt=int_dt)
+
+        goal_y_minus_start_y = pr.compact_axis_angle_from_quaternion(pr.concatenate_quaternions(goal_y, pr.q_conj(start_y)))
+
         current_ydd[:] = (
-            alpha_y * (beta_y * pr.compact_axis_angle_from_quaternion(
-                           pr.concatenate_quaternions(
-                               goal_y, pr.q_conj(current_y)))
-                       - execution_time * current_yd)
-            + f + cdd) / execution_time ** 2
+            alpha_y * (
+                beta_y * pr.compact_axis_angle_from_quaternion(pr.concatenate_quaternions(goal_y, pr.q_conj(current_y)))
+                + execution_time * goal_yd
+                - execution_time * current_yd
+                - beta_y * s * goal_y_minus_start_y
+            )
+            + goal_ydd * execution_time ** 2
+            + f
+            + cdd
+        ) / execution_time ** 2
         current_yd += dt * current_ydd + cd / execution_time
         current_y[:] = pr.concatenate_quaternions(
             pr.quaternion_from_compact_axis_angle(dt * current_yd), current_y)
@@ -457,8 +466,8 @@ def dmp_quaternion_imitation(
     forcing_term = ForcingTerm(
         3, n_weights_per_dim, T[-1], T[0], overlap, alpha_z)
     F, start_y, start_yd, start_ydd, goal_y, goal_yd, goal_ydd = \
-        determine_forces_quaternion(T, Y, alpha_y, beta_y,
-                                    allow_final_velocity)  # n_steps x n_dims
+        determine_forces_quaternion(
+            T, Y, alpha_y, beta_y, alpha_z, allow_final_velocity)  # n_steps x n_dims
 
     X = forcing_term.design_matrix(T)  # n_weights_per_dim x n_steps
 
@@ -466,7 +475,7 @@ def dmp_quaternion_imitation(
             start_y, start_yd, start_ydd, goal_y, goal_yd, goal_ydd)
 
 
-def determine_forces_quaternion(T, Y, alpha_y, beta_y, allow_final_velocity):
+def determine_forces_quaternion(T, Y, alpha_y, beta_y, alpha_z, allow_final_velocity):
     """Determine forces that the forcing term should generate.
 
     Parameters
@@ -482,6 +491,9 @@ def determine_forces_quaternion(T, Y, alpha_y, beta_y, allow_final_velocity):
 
     beta_y : float
         Parameter of the transformation system.
+
+    alpha_z : float
+        Parameter of the canonical system.
 
     allow_final_velocity : bool
         Whether a final velocity is allowed. Will be set to 0 otherwise.
@@ -524,14 +536,19 @@ def determine_forces_quaternion(T, Y, alpha_y, beta_y, allow_final_velocity):
 
     execution_time = T[-1] - T[0]
     goal_y = Y[-1]
+    goal_yd = Yd[-1]
+    goal_ydd = Ydd[-1]
+    start_y = Y[0]
+    goal_y_minus_start_y = pr.compact_axis_angle_from_quaternion(pr.concatenate_quaternions(goal_y, pr.q_conj(start_y)))
+    S = phase(T, alpha_z, T[-1], T[0])
     F = np.empty((len(T), n_dims))
     for t in range(len(T)):
-        F[t, :] = (
-            execution_time ** 2 * Ydd[t]
-            - alpha_y * (beta_y * pr.compact_axis_angle_from_quaternion(
-                             pr.concatenate_quaternions(
-                                 goal_y, pr.q_conj(Y[t])))
-                         - Yd[t] * execution_time))
+        F[t, :] = execution_time ** 2 * Ydd[t] - alpha_y * (
+            beta_y * pr.compact_axis_angle_from_quaternion(pr.concatenate_quaternions(goal_y, pr.q_conj(Y[t])))
+            + goal_yd * execution_time
+            - Yd[t] * execution_time
+            - beta_y * S[t] * goal_y_minus_start_y
+        ) - execution_time ** 2 * goal_ydd
     return F, Y[0], Yd[0], Ydd[0], Y[-1], Yd[-1], Ydd[-1]
 
 

movement_primitives/dmp/_dmp_with_final_velocity.py
@@ -3,6 +3,7 @@ from ._base import DMPBase, WeightParametersMixin
 from ._forcing_term import ForcingTerm
 from ._canonical_system import canonical_system_alpha
 from ._dmp import dmp_imitate, dmp_open_loop
+from ._forcing_term import phase
 
 
 class DMPWithFinalVelocity(WeightParametersMixin, DMPBase):
@@ -226,7 +227,7 @@ def apply_constraints(t, goal_y, goal_t, coefficients):
         return g, gd, gdd
 
 
-def determine_forces(T, Y, alpha_y, beta_y, allow_final_velocity):
+def determine_forces(T, Y, alpha_y, beta_y, alpha_z, allow_final_velocity):
     """Determine forces that the forcing term should generate.
 
     Parameters
@@ -242,6 +243,9 @@ def determine_forces(T, Y, alpha_y, beta_y, allow_final_velocity):
 
     beta_y : float
         Parameter of the transformation system.
+
+    alpha_z : float
+        Parameter of the canonical system.
 
     allow_final_velocity : bool
         Whether a final velocity is allowed. This should always be True for
@@ -289,12 +293,16 @@ def determine_forces(T, Y, alpha_y, beta_y, allow_final_velocity):
         T[0], T[-1], Y[0], Yd[0], Ydd[0], Y[-1], Yd[-1], Ydd[-1])
 
     execution_time = T[-1] - T[0]
+    S = phase(T, alpha_z, T[-1], T[0])
     F = np.empty((len(T), n_dims))
-    for i in range(len(T)):
-        g, gd, gdd = apply_constraints(T[i], Y[-1], T[-1], coefficients)
-        F[i, :] = execution_time ** 2 * Ydd[i] - alpha_y * (
-            beta_y * (g - Y[i]) + gd * execution_time
-            - Yd[i] * execution_time) - execution_time ** 2 * gdd
+    for t in range(len(T)):
+        g, gd, gdd = apply_constraints(T[t], Y[-1], T[-1], coefficients)
+        F[t, :] = execution_time ** 2 * Ydd[t] - alpha_y * (
+            beta_y * (g - Y[t])
+            + gd * execution_time
+            - Yd[t] * execution_time
+            - beta_y * (g - Y[0]) * S[t]
+        ) - execution_time ** 2 * gdd
     return F, Y[0], Yd[0], Ydd[0], Y[-1], Yd[-1], Ydd[-1]
 
 
@@ -398,13 +406,20 @@ def dmp_step_euler_with_constraints(
 
         f = forcing_term(current_t).squeeze()
 
+        s = phase(current_t, forcing_term.alpha_z, goal_t, start_t, int_dt=int_dt)
+
         g, gd, gdd = apply_constraints(current_t, goal_y, goal_t, coefficients)
 
         coupling_sum = cdd + p_gain * tracking_error / dt
-        ydd = (alpha_y * (beta_y * (g - current_y)
-                          + execution_time * gd
-                          - execution_time * current_yd)
-               + gdd * execution_time ** 2
-               + f + coupling_sum) / execution_time ** 2
+        ydd = (
+            alpha_y * (
+                beta_y * (g - current_y)
+                + execution_time * gd
+                - execution_time * current_yd
+                - beta_y * (g - start_y) * s
+            )
+            + gdd * execution_time ** 2
+            + f + coupling_sum
+        ) / execution_time ** 2
         current_yd += dt * ydd + cd / execution_time
         current_y += dt * current_yd

movement_primitives/dmp_potential_field.py
@@ -1,6 +1,7 @@
 """Visualization of DMP as potential field."""
 import numpy as np
-from .dmp import dmp_transformation_system, obstacle_avoidance_acceleration_2d
+from .dmp import (
+    dmp_transformation_system, obstacle_avoidance_acceleration_2d, phase)
 
 
 def potential_field_2d(dmp, x_range, y_range, n_ticks, obstacle=None):
@@ -50,9 +51,11 @@ def potential_field_2d(dmp, x_range, y_range, n_ticks, obstacle=None):
     Yd = np.empty_like(Y)
     Yd[:, :] = dmp.current_yd
 
+    s = phase(dmp.t, dmp.forcing_term.alpha_z, dmp.execution_time_, 0.0)
+
     ts = dmp_transformation_system(
         Y, Yd, dmp.alpha_y, dmp.beta_y, dmp.goal_y, dmp.goal_yd, dmp.goal_ydd,
-        dmp.execution_time_)
+        dmp.start_y, s, dmp.execution_time_)
     ft = np.empty_like(ts)
     ft[:, :] = (dmp.forcing_term(dmp.t) / dmp.execution_time_ ** 2).ravel()
 