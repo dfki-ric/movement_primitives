51a4035b8f42753d13a1a7d2524b56bd4d5e4835
Alexander Fabisch <alexander.fabisch@dfki.de>
2023-11-18 11:30:44 +0100

    Fix unit test

--- test/test_step_dual_cartesian.py
+++ test/test_step_dual_cartesian.py
@@ -1,18 +1,25 @@
+from copy import deepcopy
 import numpy as np
 from numpy.testing import assert_array_almost_equal
+from movement_primitives.dmp._forcing_term import ForcingTerm
+from movement_primitives.dmp._canonical_system import canonical_system_alpha
 
 
 def test_compare_python_cython():
-    from copy import deepcopy
     from movement_primitives.dmp._dual_cartesian_dmp import dmp_step_dual_cartesian_python
     from movement_primitives.dmp_fast import dmp_step_dual_cartesian as dmp_step_dual_cartesian_cython
+    alpha_z = canonical_system_alpha(0.01, 2.0, 0.0, 0.001)
+    forcing_term = ForcingTerm(12, 10, 2.0, 0.0, 0.8, alpha_z)
     kwargs = dict(
         last_t=1.999, t=2.0,
-        current_y=np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]), current_yd=np.zeros([12]),
-        goal_y=np.array([1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0]), goal_yd=np.zeros([12]), goal_ydd=np.zeros([12]),
-        start_y=np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]), start_yd=np.zeros([12]), start_ydd=np.zeros([12]),
+        current_y=np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]),
+        current_yd=np.zeros([12]),
+        goal_y=np.array([1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0]),
+        goal_yd=np.zeros([12]), goal_ydd=np.zeros([12]),
+        start_y=np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]),
+        start_yd=np.zeros([12]), start_ydd=np.zeros([12]),
         goal_t=2.0, start_t=0.0, alpha_y=25.0, beta_y=6.25,
-        forcing_term=lambda x: 10000 * np.ones((12, 1)), coupling_term=None, int_dt=0.0001,
+        forcing_term=forcing_term, coupling_term=None, int_dt=0.0001,
         p_gain=0.0, tracking_error=None
     )
     kwargs_python = deepcopy(kwargs)
