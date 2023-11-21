1ee323353dd8933cb0dd62b163ba05a022da4781
Alexander Fabisch <alexander.fabisch@dfki.de>
2023-11-21 16:30:20 +0100

    Test temporal scaling of dual Cart. DMP

--- test/test_dual_cartesian_dmp.py
+++ test/test_dual_cartesian_dmp.py
@@ -1,5 +1,6 @@
+import numpy as np
 from movement_primitives.dmp import DualCartesianDMP
-from nose.tools import assert_raises_regexp
+from nose.tools import assert_raises_regexp, assert_less
 
 
 def test_invalid_step_function():
@@ -7,3 +8,31 @@ def test_invalid_step_function():
         execution_time=1.0, dt=0.01, n_weights_per_dim=10, int_dt=0.001)
     assert_raises_regexp(ValueError, "Step function", dmp.open_loop,
                          step_function="invalid")
+
+
+def test_temporal_scaling():
+    execution_time = 2.0
+    dt = 0.01
+
+    dmp = DualCartesianDMP(
+        execution_time=execution_time, dt=dt, n_weights_per_dim=100)
+
+    T = np.arange(0.0, execution_time + dt, dt)
+    Y_demo = np.zeros((len(T), 14))
+    Y_demo[:, 0] = np.cos(2.5 * np.pi * T)
+    Y_demo[:, 1] = 0.5 + np.cos(1.5 * np.pi * T)
+    Y_demo[:, 3] = 1.0
+    Y_demo[:, 10] = 1.0
+    dmp.imitate(T, Y_demo)
+
+    dmp.configure(start_y=Y_demo[0], goal_y=Y_demo[-1])
+    _, Y2 = dmp.open_loop()
+
+    dmp.execution_time_ = 1.0
+    _, Y1 = dmp.open_loop()
+
+    dmp.execution_time_ = 4.0
+    _, Y4 = dmp.open_loop()
+
+    assert_less(np.linalg.norm(Y1 - Y2[::2]) / len(Y1), 1e-3)
+    assert_less(np.linalg.norm(Y2 - Y4[::2]) / len(Y2), 1e-3)
