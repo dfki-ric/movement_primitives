f8ce027f7adf005abda5d7b5c3873fe4af2d8a88
Alexander Fabisch <alexander.fabisch@dfki.de>
2023-11-21 17:03:58 +0100

    Test stepwise execution of state-following DMP

--- test/test_state_following_dmp.py
+++ test/test_state_following_dmp.py
@@ -23,3 +23,28 @@ def test_state_following_dmp2d():
         viapoint = dmp.forcing_term.viapoints[i]
         min_dist = np.min(np.linalg.norm(Y - viapoint[np.newaxis], axis=1))
         assert_less(min_dist, 0.02)
+
+
+def test_state_following_dmp2d_stepwise():
+    start_y = np.array([0.0, 1.0])
+    dt = 0.001
+    execution_time = 3.0
+    n_viapoints = 8
+
+    dmp = StateFollowingDMP(n_dims=2, execution_time=execution_time, dt=dt, n_viapoints=n_viapoints)
+    dmp.forcing_term.viapoints[:, 0] = np.linspace(0, 1, n_viapoints)
+    dmp.forcing_term.viapoints[:, 1] = np.linspace(1, 2, n_viapoints)
+    dmp.configure(start_y=start_y)
+
+    current_y = np.copy(dmp.start_y)
+    current_yd = np.copy(dmp.start_yd)
+    Y = [np.copy(current_y)]
+    for _ in np.arange(0.0, execution_time + dt, dt):
+        current_y, current_yd = dmp.step(current_y, current_yd)
+        Y.append(np.copy(current_y))
+    Y = np.array(Y)
+
+    for i in range(n_viapoints):
+        viapoint = dmp.forcing_term.viapoints[i]
+        min_dist = np.min(np.linalg.norm(Y - viapoint[np.newaxis], axis=1))
+        assert_less(min_dist, 0.02)
