f6559683da1c07c2c5f7ae42f2b923bf96641ea0
Alexander Fabisch <alexander.fabisch@dfki.de>
2023-11-21 16:09:39 +0100

    Test invalid regularization

--- test/test_cartesian_dmp.py
+++ test/test_cartesian_dmp.py
@@ -1,5 +1,6 @@
 import numpy as np
 from movement_primitives.dmp import CartesianDMP
+from movement_primitives.dmp._cartesian_dmp import dmp_quaternion_imitation
 from pytransform3d import rotations as pr
 from numpy.testing import assert_array_almost_equal, assert_raises_regex
 from nose.tools import assert_equal, assert_less
@@ -161,3 +162,32 @@ def test_temporal_scaling():
 
     assert_less(np.linalg.norm(Y1 - Y2[::2]) / len(Y1), 1e-3)
     assert_less(np.linalg.norm(Y2 - Y4[::2]) / len(Y2), 1e-3)
+
+
+def test_invalid_regularization_coefficient():
+    execution_time = 2.0
+    dt = 0.01
+
+    dmp = CartesianDMP(
+        execution_time=execution_time, dt=dt,
+        n_weights_per_dim=10, int_dt=0.0001)
+    Y = np.zeros((1001, 7))
+    T = np.linspace(0, 1, len(Y))
+    sigmoid = 0.5 * (np.tanh(1.5 * np.pi * (T - 0.5)) + 1.0)
+    Y[:, 0] = 0.6
+    Y[:, 1] = -0.2 + 0.4 * sigmoid
+    Y[:, 2] = 0.45
+    start_aa = np.array([0.0, 1.0, 0.0, 0.25 * np.pi])
+    goal_aa = np.array([0.0, 0.0, 1.0, 0.25 * np.pi])
+    for t in range(len(Y)):
+        frac = sigmoid[t]
+        aa_t = (1.0 - frac) * start_aa + frac * goal_aa
+        aa_t[:3] /= np.linalg.norm(aa_t[:3])
+        Y[t, 3:] = pr.quaternion_from_axis_angle(aa_t)
+    assert_raises_regex(
+        ValueError, "Regularization coefficient", dmp_quaternion_imitation,
+        T, Y[:, 3:], n_weights_per_dim=dmp.n_weights_per_dim,
+        regularization_coefficient=-1.0, alpha_y=dmp.alpha_y, beta_y=dmp.beta_y,
+        overlap=dmp.forcing_term_rot.overlap,
+        alpha_z=dmp.forcing_term_rot.alpha_z,
+        allow_final_velocity=True)
