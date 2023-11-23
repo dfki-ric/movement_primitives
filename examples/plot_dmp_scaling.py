63aca5a660a7bb564c6a219cb536f851a045d086
Alexander Fabisch <alexander.fabisch@dfki.de>
2023-11-23 18:33:29 +0100

    Test scaling of DMPs

--- examples/plot_dmp_scaling.py
+++ examples/plot_dmp_scaling.py
@@ -6,17 +6,20 @@ Spatial Scaling of DMP
 The standard DMP definition of Ijspeert et al. (2013) does not scale well,
 when the original demonstration has both the start and the goal state close
 together: small perturbation in the start and/or goal state often result in
-large accelerations. '''To fix this issue, our implementation of the DMP does
-not scale the forcing term by $(g - y_0)$!''' This example demonstrates the
-behavior of this DMP implementation in these cases. We adopted the modification
-of
+large accelerations. To fix this issue, our implementation of the DMP does
+not scale the forcing term by $(g - y_0)$. We also adopted the modification of
 
 P. Pastor, H. Hoffmann, T. Asfour, S. Schaal:
 Learning and Generalization of Motor Skills by Learning from Demonstration
+
+Note that this has to be activated explicitly by setting smooth scaling in the
+constructor of the DMP. This behavior is implemented for all types of DMPs.
+This example demonstrates the behavior of this DMP implementation in these
+cases.
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from movement_primitives.dmp import DMP, DMPWithFinalVelocity, CartesianDMP, DualCartesianDMP
+from movement_primitives.dmp import DMP
 
 
 T = np.linspace(0, 1, 101)
@@ -29,19 +32,20 @@ start = Y[0]
 goal = Y[-1]
 new_start = np.array([0.5, 0.5, 0, 1, 0, 0, 0, 0.5, 0.5, 0, 1, 0, 0, 0])
 new_goal = np.array([-0.5, 0.5, 0, 1, 0, 0, 0, -0.5, 0.5, 0, 1, 0, 0, 0])
-Y_shifted = Y - goal[np.newaxis] + new_goal[np.newaxis]
+Y_start_shifted = Y - start[np.newaxis] + new_start[np.newaxis]
+Y_goal_shifted = Y - goal[np.newaxis] + new_goal[np.newaxis]
 
-#dmp = DMP(n_dims=len(start), execution_time=1.0, dt=0.01, n_weights_per_dim=20, smooth_scaling=False)
-#dmp = DMPWithFinalVelocity(n_dims=len(start), execution_time=1.0, dt=0.01, n_weights_per_dim=20, smooth_scaling=True)
-#dmp = CartesianDMP(execution_time=1.0, dt=0.01, n_weights_per_dim=20, smooth_scaling=False)
-dmp = DualCartesianDMP(execution_time=1.0, dt=0.01, n_weights_per_dim=20, smooth_scaling=False)
+dmp = DMP(n_dims=len(start), execution_time=1.0, dt=0.01, n_weights_per_dim=20,
+          smooth_scaling=True)
 dmp.imitate(T, Y)
 dmp.configure(start_y=new_start, goal_y=new_goal)
-_, Y_dmp = dmp.open_loop(step_function="cython")
+_, Y_dmp = dmp.open_loop()
 
 plt.plot(Y[:, 0], Y[:, 1], label=r"Demonstration, $g \approx y_0$", ls="--")
-plt.plot(Y_shifted[:, 0], Y_shifted[:, 1], label="Original shape with new goal", ls="--")
+plt.plot(Y_start_shifted[:, 0], Y_start_shifted[:, 1], label="Original shape with new start", ls="--")
+plt.plot(Y_goal_shifted[:, 0], Y_goal_shifted[:, 1], label="Original shape with new goal", ls="--")
 plt.plot(Y_dmp[:, 0], Y_dmp[:, 1], label="DMP with new goal", lw=5, alpha=0.5)
+plt.scatter(Y_dmp[:, 0], Y_dmp[:, 1], alpha=0.5)
 plt.scatter(start[0], start[1], c="r", label="Goal of demonstration: $g$")
 plt.scatter(goal[0], goal[1], c="g", label="Start of demonstration: $y_0$")
 plt.scatter(new_start[0], new_start[1], c="c", label="New start: $y_0'$")
---
+++ test/test_dmp_smooth_spatial_scaling.py
@@ -0,0 +1,104 @@
+import numpy as np
+from pytransform3d.rotations import quaternion_gradient, assert_quaternion_equal
+from movement_primitives.dmp import (
+    DMP, DMPWithFinalVelocity, CartesianDMP, DualCartesianDMP)
+from nose.tools import assert_less
+from numpy.testing import assert_array_almost_equal
+
+
+def _setup_circle():
+    T = np.linspace(0, 1, 101)
+    x = np.sin(T ** 2 * 1.99 * np.pi)
+    y = np.cos(T ** 2 * 1.99 * np.pi)
+    return T, x, y
+
+
+def test_smooth_scaling_dmp():
+    T, x, y = _setup_circle()
+    Y = np.column_stack((x, y))
+    new_start, new_goal = np.array([5.0, 0.5]), np.array([-5.0, 0.5])
+
+    dmp = DMP(n_dims=2, execution_time=1.0, dt=0.01,
+              n_weights_per_dim=20, smooth_scaling=False)
+    dmp.imitate(T, Y)
+    dmp.configure(start_y=new_start, goal_y=new_goal)
+    for step_function in ["rk4", "euler", "euler-cython", "rk4-cython"]:
+        dmp.smooth_scaling = False
+        _, Y_dmp = dmp.open_loop(step_function=step_function)
+        dmp.smooth_scaling = True
+        _, Y_dmp_smooth = dmp.open_loop(step_function=step_function)
+        Yd_dmp = np.gradient(Y_dmp, axis=0)
+        Ydd_dmp = np.gradient(Yd_dmp, axis=0)
+        Yd_dmp_smooth = np.gradient(Y_dmp_smooth, axis=0)
+        Ydd_dmp_smooth = np.gradient(Yd_dmp_smooth, axis=0)
+        # with smooth scaling we avoid large accelerations in the beginning
+        assert_less(np.max(np.abs(Ydd_dmp_smooth)), 0.2 * np.max(np.abs(Ydd_dmp)))
+        assert_array_almost_equal(Y_dmp[-1], new_goal, decimal=1)
+
+
+def test_smooth_scaling_cartesian_dmp():
+    T, x, y = _setup_circle()
+    z = qx = qy = qz = np.zeros_like(x)
+    qw = np.ones_like(x)
+    Y = np.column_stack((x, y, z, qw, qx, qy, qz))
+    new_start = np.array([5.0, 0.5, 0, 0, 0, 1, 0])
+    new_goal = np.array([-5.0, 0.5, 0, 0, 1, 0, 0])
+
+    dmp = CartesianDMP(execution_time=1.0, dt=0.01, n_weights_per_dim=20, smooth_scaling=False)
+    dmp.imitate(T, Y)
+    dmp.configure(start_y=new_start, goal_y=new_goal)
+    for step_function in ["cython", "python"]:
+        dmp.smooth_scaling = False
+        _, Y_dmp = dmp.open_loop(quaternion_step_function=step_function)
+        dmp.smooth_scaling = True
+        _, Y_dmp_smooth = dmp.open_loop(quaternion_step_function=step_function)
+        Yd_pos_dmp = np.gradient(Y_dmp[:, :3], axis=0)
+        Ydd_pos_dmp = np.gradient(Yd_pos_dmp[:, :3], axis=0)
+        Yd_pos_dmp_smooth = np.gradient(Y_dmp_smooth, axis=0)
+        Ydd_pos_dmp_smooth = np.gradient(Yd_pos_dmp_smooth, axis=0)
+        Yd_orn_dmp = quaternion_gradient(Y_dmp[:, 3:])
+        Ydd_orn_dmp = np.gradient(Yd_orn_dmp, axis=0)
+        Yd_orn_dmp_smooth = quaternion_gradient(Y_dmp_smooth[:, 3:])
+        Ydd_orn_dmp_smooth = np.gradient(Yd_orn_dmp_smooth, axis=0)
+        # with smooth scaling we avoid large accelerations in the beginning
+        assert_less(np.max(np.abs(Ydd_pos_dmp_smooth)),
+                    0.2 * np.max(np.abs(Ydd_pos_dmp)))
+        assert_less(np.max(np.abs(Ydd_orn_dmp_smooth)),
+                    0.2 * np.max(np.abs(Ydd_orn_dmp)))
+        assert_array_almost_equal(Y_dmp[-1, :3], new_goal[:3], decimal=1)
+        assert_quaternion_equal(Y_dmp[-1, 3:], new_goal[3:], decimal=4)
+
+
+def test_smooth_scaling_dual_cartesian_dmp():
+    T, x, y = _setup_circle()
+    z = qx = qy = qz = np.zeros_like(x)
+    qw = np.ones_like(x)
+    Y = np.column_stack((x, y, z, qw, qx, qy, qz, x, y, z, qw, qx, qy, qz))
+    new_start = np.array([5.0, 0.5, 0, 0, 0, 1, 0, 5.0, 0.5, 0, 0, 0, 1, 0])
+    new_goal = np.array([-5.0, 0.5, 0, 0, 1, 0, 0, -5.0, 0.5, 0, 0, 1, 0, 0])
+
+    dmp = DualCartesianDMP(
+        execution_time=1.0, dt=0.01, n_weights_per_dim=20,
+        smooth_scaling=False)
+    dmp.imitate(T, Y)
+    dmp.configure(start_y=new_start, goal_y=new_goal)
+    for step_function in ["cython", "python"]:
+        dmp.smooth_scaling = False
+        _, Y_dmp = dmp.open_loop(step_function=step_function)
+        dmp.smooth_scaling = True
+        _, Y_dmp_smooth = dmp.open_loop(step_function=step_function)
+        Yd_pos_dmp = np.gradient(Y_dmp[:, :3], axis=0)
+        Ydd_pos_dmp = np.gradient(Yd_pos_dmp[:, :3], axis=0)
+        Yd_pos_dmp_smooth = np.gradient(Y_dmp_smooth, axis=0)
+        Ydd_pos_dmp_smooth = np.gradient(Yd_pos_dmp_smooth, axis=0)
+        Yd_orn_dmp = quaternion_gradient(Y_dmp[:, 3:7])
+        Ydd_orn_dmp = np.gradient(Yd_orn_dmp, axis=0)
+        Yd_orn_dmp_smooth = quaternion_gradient(Y_dmp_smooth[:, 3:7])
+        Ydd_orn_dmp_smooth = np.gradient(Yd_orn_dmp_smooth, axis=0)
+        # with smooth scaling we avoid large accelerations in the beginning
+        assert_less(np.max(np.abs(Ydd_pos_dmp_smooth)),
+                    0.2 * np.max(np.abs(Ydd_pos_dmp)))
+        assert_less(np.max(np.abs(Ydd_orn_dmp_smooth)),
+                    0.2 * np.max(np.abs(Ydd_orn_dmp)))
+        assert_array_almost_equal(Y_dmp[-1, :3], new_goal[:3], decimal=1)
+        assert_quaternion_equal(Y_dmp[-1, 3:7], new_goal[3:7], decimal=4)
