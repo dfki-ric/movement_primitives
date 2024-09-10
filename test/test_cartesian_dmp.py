fd30228b9cdb488bff4c4882dce32acb42e4786b
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2024-09-10 16:33:29 +0100

    Modify tests to account for vectorization of alpha_y and beta_y

--- test/test_cartesian_dmp.py
+++ test/test_cartesian_dmp.py
@@ -75,7 +75,7 @@ def test_compare_python_cython():
         current_y=np.array([1.0, 0.0, 0.0, 0.0]), current_yd=np.zeros([3]),
         goal_y=np.array([1.0, 0.0, 0.0, 0.0]), goal_yd=np.zeros([3]), goal_ydd=np.zeros([3]),
         start_y=np.array([1.0, 0.0, 0.0, 0.0]), start_yd=np.zeros([3]), start_ydd=np.zeros([3]),
-        goal_t=2.0, start_t=0.0, alpha_y=25.0, beta_y=6.25,
+        goal_t=2.0, start_t=0.0, alpha_y=25.0*np.ones(3), beta_y=6.25*np.ones(3),
         forcing_term=forcing_term, coupling_term=None, int_dt=0.0001
     )
     kwargs_python = deepcopy(kwargs)
