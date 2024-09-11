579f02c827952436291d333190d3a2fb18dbfd08
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2024-09-11 16:08:45 +0100

    More consistent and appropriate variable names

--- movement_primitives/utils.py
+++ movement_primitives/utils.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def ensure_1d_array(value, dim, label):
+def ensure_1d_array(value, n_dims, var_name):
     """Process scalar or array-like input to ensure it is a 1D numpy array of the correct shape."""
     value = np.atleast_1d(value).astype(float).flatten()
     if value.shape[0] == 1:
-        value = value * np.ones(dim)
-    elif value.shape != (dim,):
-        raise ValueError(f"{label} has incorrect shape, expected ({dim},) got {value.shape}")
+        value = value * np.ones(n_dims)
+    elif value.shape != (n_dims,):
+        raise ValueError(f"{var_name} has incorrect shape, expected ({n_dims},) got {value.shape}")
     return value
